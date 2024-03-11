84693b05dda4c1d191480fabd6e37a0494fb0b50
P-Long <longpham2911@gmail.com>
2024-03-11 23:43:47 +0700

    refs:D2-6

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Backend\VaiTroController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\ExportController;
-use App\Http\Controllers\Backend\LoaiPhongController;
+use App\Http\Controllers\Backend\DonDatController;
 
 /*
 |--------------------------------------------------------------------------
