b66b30fdc7cf5e6bf8172cb57122b58305ac4f2f
namvv <namvv@gmail.com>
2024-03-11 23:01:46 +0700

    refs:D2-18

--- app/Http/Controllers/Backend/DonDatController.php
+++ app/Http/Controllers/Backend/DonDatController.php
@@ -17,5 +17,34 @@ class DonDatController extends Controller
         return view(self::PATH_VIEW . __FUNCTION__, compact('dondat'));
     }
 
+    public function create()
+    {
+        return view(self::PATH_VIEW . __FUNCTION__);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'ma_phong' => 'required|unique:don_dats|max:225',
+            'ngay_dat' => 'date',
+            'khach_hang_id' => 'required|unique:don_dats|max:225',
+            'payment' => 'required|max:225',
+            'ghi_chu' => 'nullable',
+            'trang_thai' => [
+                'required',
+                Rule::in([
+                    Don_dat::DA_XAC_NHAN,
+                    Don_dat::CHO_XAC_NHAN
+                ])
+            ],
+        ]);
+
+        Don_dat::query()->create($request->all());
+
+        return back()->with('msg', 'Add Success');
+    }
+
+
+
 
 }
