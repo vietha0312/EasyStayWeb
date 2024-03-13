173876a88a9fec469fd166a8aece2c815bd071cb
Jungx <dungnnph20620@fpt.edu.vn>
2024-03-13 20:09:16 +0700

    crud-danh_gia

--- resources/views/admin/layouts/components/sidebar.blade.php
+++ resources/views/admin/layouts/components/sidebar.blade.php
@@ -99,19 +99,18 @@
 
                 <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon bi bi-box-seam-fill"></i>
                         <p>
-                            Quản lý chi tiết đơn đặt phòng
+                            Quản lý Banner
                             <i class="nav-arrow bi bi-chevron-right"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
-                        <li class="nav-item"><a href="{{ route('admin.chi_tiet_don_dat.index') }}" class="nav-link"><i
-                                    class="nav-icon bi bi-circle"></i>
+                        <li class="nav-item"><a href="{{route('admin.banners.index')}}" class="nav-link"><i class="nav-icon bi bi-circle"></i>
                                 <p>Danh sách</p>
                             </a></li>
-                        <li class="nav-item"><a href="{{ route('admin.chi_tiet_don_dat.create') }}" class="nav-link"><i
-                                    class="nav-icon bi bi-circle"></i>
+                        <li class="nav-item"><a href="{{route('admin.banners.create')}}" class="nav-link"><i class="nav-icon bi bi-circle"></i>
                                 <p>Tạo mới</p>
                             </a></li>
+                          
 
                     </ul>
                 </li>
