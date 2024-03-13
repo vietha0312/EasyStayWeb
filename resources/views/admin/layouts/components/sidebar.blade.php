2ad182d7893497a1bf1eddbec4321cbbcc330788
namvv <namvv@gmail.com>
2024-03-13 16:00:33 +0700

    refs:D2-28 giao diện bảng chị tiết đơn đặt phòng

--- resources/views/admin/layouts/components/sidebar.blade.php
+++ resources/views/admin/layouts/components/sidebar.blade.php
@@ -16,6 +16,7 @@
                         </p>
                     </a>
 
+                
 
                 <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon bi bi-table"></i>
                         <p>
@@ -84,6 +85,23 @@
 
                     </ul>
                 </li>
+
+                <li class="nav-item"><a href="#" class="nav-link"><i class="nav-icon bi bi-box-seam-fill"></i>
+                    <p>
+                        Quản lý chi tiết đơn đặt phòng
+                        <i class="nav-arrow bi bi-chevron-right"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item"><a href="{{ route('admin.chi_tiet_don_dat.index') }}" class="nav-link"><i class="nav-icon bi bi-circle"></i>
+                            <p>Danh sách</p>
+                        </a></li>
+                    <li class="nav-item"><a href="{{ route('admin.chi_tiet_don_dat.create') }}" class="nav-link"><i class="nav-icon bi bi-circle"></i>
+                            <p>Tạo mới</p>
+                        </a></li>
+
+                </ul>
+            </li>
 
                 <li class="nav-item"><a href="{{ route('admin.chi_tiet_don_dat.index') }}" class="nav-link"><i class="nav-icon bi bi-box-seam-fill"></i>
                         <p>
@@ -195,7 +213,7 @@
 
                     </ul>
                 </li>
-
+            
             </ul><!--end::Sidebar Menu-->
         </nav>
     </div><!--end::Sidebar Wrapper-->
