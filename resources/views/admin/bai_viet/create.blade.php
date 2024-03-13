35ba6a9879a94d44455dee0c2e098c637ad78d4b
vietha0312 <pvietha301@gmail.com>
2024-03-13 20:07:48 +0700

    fix_giao_dien

--- resources/views/admin/bai_viet/create.blade.php
+++ resources/views/admin/bai_viet/create.blade.php
@@ -1,27 +1,51 @@
 @extends('admin.layouts.master')
+
 @section('content')
-    <form class="m-3" action="{{ route('admin.bai_viet.store') }}" method="post" enctype="multipart/form-data">
-        @csrf
-        <h2>Thêm mới bài viết</h2>
-        <label for="tieu_de">Tiêu đề</label>
-        <input type="text" name="tieu_de" id="tieu_de" class="form-control">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Thêm mới bài viết</h2>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('admin.bai_viet.store') }}" method="post" enctype="multipart/form-data">
+                            @csrf
 
-        <label class="mt-3" for="anh">Ảnh</label>
-        <input type="file" name="anh" id="anh" class="form-control">
+                            <div class="mb-3">
+                                <label for="tieu_de" class="form-label">Tiêu đề:</label>
+                                <input type="text" name="tieu_de" id="tieu_de" class="form-control">
+                            </div>
 
+                            <div class="mb-3">
+                                <label for="anh" class="form-label">Ảnh:</label>
+                                <input type="file" name="anh" id="anh" class="form-control">
+                            </div>
 
-        <label class="mt-3" for="noi_dung">Nội dung</label>
-        <textarea name="noi_dung" id="noi_dung" cols="30" rows="10" class="form-control"></textarea>
+                            <div class="mb-3">
+                                <label for="noi_dung" class="form-label">Nội dung:</label>
+                                <textarea name="noi_dung" id="noi_dung" cols="30" rows="10" class="form-control"></textarea>
+                            </div>
 
-        <label class="mt-3" for="trang_thai">Trạng thái:</label>
+                            <div class="mb-3">
+                                <label class="form-label">Trạng thái:</label> <br>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="trang_thai" id="trang_thai1"
+                                        value="{{ \App\Models\Bai_viet::XUAT_BAN }}">
+                                    <label class="form-check-label" for="trang_thai1">Xuất bản</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="trang_thai" id="trang_thai2"
+                                        value="{{ \App\Models\Bai_viet::NHAP }}">
+                                    <label class="form-check-label" for="trang_thai2">Nháp</label>
+                                </div>
+                            </div>
 
-        <input type="radio" name="trang_thai" id="trang_thai1" value="{{ \App\Models\Bai_viet::XUAT_BAN }}">
-        <label for="trang_thai1">XUẤT BẢN</label>
-
-        <input type="radio" name="trang_thai" id="trang_thai2" value="{{ \App\Models\Bai_viet::NHAP }}">
-        <label for="trang_thai2">NHÁP</label> <br><br>
-
-        <button type="submit" class="btn btn-success mt-3">GỬI</button>
-
-    </form>
+                            <button type="submit" class="btn btn-success">Gửi</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
